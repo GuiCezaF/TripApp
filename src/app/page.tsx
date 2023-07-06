30ed00286606d198139369abb63c6207543b683c
Guilherme Cezarino Felipe <124943256+GuiCezaF@users.noreply.github.com>
2023-07-06 12:16:57 -0300

    feat: add initial search trip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 'use client'
 
+import TripSearch from "./components/TripSearch";
+
 export default function Home() {
   return (
-    <div className="bg-primary">
-
+    <div >
+      <TripSearch/>
     </div>
   );
 }
